3ef7f20ab11ecd6a7f4828a59f738f01503b0d6f
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-03-24 20:06:35 +0100

    Implented arg parsing for user prompt and verbosity

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+import argparse
+from ast import arg
 import os
+from argparse import ArgumentParser
+import threading
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
-    print("Hello from aiagent!")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
     client = genai.Client(api_key=api_key)
-
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
+
+    metadata = response.usage_metadata
+    assert metadata is not None
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {metadata.prompt_token_count}")
+        print(f"Response tokens: {metadata.candidates_token_count}")
 
     print(response.text)
 
